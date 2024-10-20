f3cda9dfa651c13cd94aa675f0cd40c0e4ad47ce
volgograd222 <147680352+volgograd222@users.noreply.github.com>
2024-10-20 11:50:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from PyQt6.QtWidgets import QApplication
+from app.mainWin import MainWin
+import sys
+import traceback
+
+
+def main():
+    app = QApplication([])
+    win = MainWin()
+    win.show()
+    sys.exit(app.exec())
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+
+
+sys.excepthook = excepthook
+
+if __name__ == '__main__':
+    main()
